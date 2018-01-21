9e19f9ed33a94e947d4150d98efec44552d8d188
Sharky! <jdeurt@gmail.com>
2018-01-21 13:19:26 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@ echo This script will install everything you need to get this bot running.
 
 cd /d %~dp0
 npm install discord.js
+npm install nodemon -g
 
 echo Done! You may close this window now.
 pause
